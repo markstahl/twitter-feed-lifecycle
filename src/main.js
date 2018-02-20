3ff059a7d7d367cc19038f55ad74518fd0de1d6f
Mark Stahl <markstahl@Marks-MacBook-Pro.local>
2018-02-20 15:24:25 -0500

    First commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TwitterFeed from './components/TwitterFeed';
+import data from './constants/data';
+
+ReactDOM.render(
+  <TwitterFeed tweets={data} />,
+  document.getElementById('app')
+);
